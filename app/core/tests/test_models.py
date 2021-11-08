e393f03b5147518d0b1a51f1b31ec205af1cac14
Juan Du <juan.du@steadymd.com>
2021-11-07 17:17:32 -0800

    finish section 7

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -23,3 +23,11 @@ class ModelTests(TestCase):
         """Test creating user with no email raises error"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, "text123")
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@londonappdev.com', 'test123'
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
